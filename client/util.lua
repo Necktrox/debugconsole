ed089bb7c7dca4a7a696a87a5c9b4738aa9a8fe9
Marek Kulik <necktrox@gmail.com>
2014-11-20 00:59:28 +0100

    Fixed util.lua functions
    Function "class" should work now and allow both ways to create an
    instance - either using the class name or using the method
    classname:create
    Also removed bloatcode from function "static"

--- client/util.lua
+++ client/util.lua
@@ -16,22 +16,38 @@ function class(name)
     -- Metamethod __call overwrite
     metatable.__call = function (self, ...)
         -- Continue when the method 'create' exists
-        if (type(new.create) == "function") then
-            -- Create an instance
-            local instance = new:create(...)
-
-            -- Run the constructor
-            if (type(instance.construct) == "function") then
-                instance:construct()
-            end
-
-            -- Return the instance
-            return instance
+        if (type(self.create) == "function") then
+            return self:create(...)
         end
     end
 
-    -- Metamethod __index overwrite
-    metatable.__index = new
+    -- Metamethod __newindex overwrite (for late-method-binding for 'create')
+    metatable.__newindex = function (self, k, v)
+        -- Continue when we create the index 'create'
+        if (type(k) == "string" and type(v) == "function" and k == "create") then
+            -- Create the actual 'create' function
+            return rawset(self, k,
+                function (self, ...)
+                    -- Create an instance
+                    local instance = v(self, ...)
+
+                    -- Bind the instance to the class
+                    setmetatable(instance, {__index = self})
+
+                    -- Run the constructor
+                    if (type(instance.construct) == "function") then
+                        instance:construct()
+                    end
+
+                    -- Return the instance
+                    return instance
+                end
+            )
+        else
+            -- Apply the new value otherwise
+            return rawset(self, k, v)
+        end
+    end
 
     -- Set the metatable for the class
     setmetatable(new, metatable)
@@ -44,18 +60,9 @@ function static(name)
     -- Verify the name
     assert(type(name) == "string" and #name > 0, "invalid name for a class")
 
-    -- Create the class table
+    -- Create the static table
     local new = {}
 
-    -- Create the metatable
-    local metatable = {}
-
-     -- Metamethod __index overwrite
-    metatable.__index = new
-
-    -- Set the metatable for the class
-    setmetatable(new, metatable)
-
-    -- Create the class in the global environment
+    -- Create the static in the global environment
     _G[name] = new
 end
